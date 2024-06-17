842d249042eb10869f5a6e357f654d92c687f5c2
Hilton Girardi Presa <hiltongp@gmail.com>
2024-06-17 20:42:39 -0300

    modifica a frase

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>Te desejo muita força e foco nas aulas de Italiano!</h1>
+    return <h1>Te desejo muita força e foco nas aulas de Italiano! :)</h1>
 }
 
 export default Home;
